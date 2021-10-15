08030a76b74cf142931de6b9e23aa91d6fc320a8
miguel <miguel.saavedra1601@alumnos.ubiobio.cl>
2021-10-15 16:12:08 -0300

    leer y escribir las img enontradas en el archivo input.html

---
+++ read.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#include <stdint.h>
+#include <regex.h>
+
+char* leer();
+int match(char * text, char * exp );
+int match(char * text, char * exp );
+int matchA(char * text, char * exp );
+void procesoImg(char * text);
+void procesoScript(char * text);
+void procesoA(char * text);
+
+int main(void){
+
+    char * text = leer(&text);
+
+    procesoImg(text);
+
+}
+
+void procesoImg(char * text){
+
+    char * exp = "<img\\s+((\\s*[^\"<>]+)\\s*=\\s*(\"[^\"<>]*\"\\s*))*\\s*>";
+
+    int numero = matchA(text, exp);
+
+    printf("las coincidencias son %d\n", numero);
+
+}
+
+void procesoScript(char * text){
+
+    char * exp = "falta expresion regular";
+
+    match(text, exp);
+
+}
+
+void procesoA(char * text){
+    char * exp = "falta expresion regular";
+    
+    match(text, exp);
+
+}
+
+char* leer() {
+    
+    const char* filename = "input.html";
+
+    FILE* input_file = fopen(filename, "r");
+    if (!input_file)
+        return "";
+
+    struct stat sb;
+    if (stat(filename, &sb) == -1) {
+        perror("stat");
+        return "";
+    }
+
+    char* file_contents = (char *)malloc(sb.st_size);
+
+    
+    fread(file_contents, sb.st_size, 1, input_file);
+
+    //printf("el texto s %s\n", file_contents);
+
+    fclose(input_file);
+
+    return file_contents;
+}
+
+
+
+#define ARRAY_SIZE(arr) (sizeof((arr)) / sizeof((arr)[0]))
+
+static const char *const str = "<img height=\"\" alt=\"1MDN\" srcset=\"mdn-logo-HD.png 2x\"><img src=\"\" alt=\"2MDN\" srcset=\"mdn-logo-HD.png 2x\"><img src=\"\" alt=\"3MDN\" srcset=\"mdn-logo-HD.png 2x\"><img src=\"\" data-deferred=\"1\" class=\"rg_i Q4LuWd\" jsname=\"Q4LuWd\" width=\"169\" height=\"200\" alt=\"Holanda - Wikipedia, la enciclopedia libre\" data-iml=\"1468.5\" data-atf=\"true\"><img src=\"dsgfdhgf\" alt=\"DA - Home | Facebook\" jsname=\"HiaYvf\" jsaction=\"load:XAeZkd;\" class=\"n3VNCb\" data-noaft=\"1\" style=\"width: 385px; height: 385.664px; margin: 0px;\">";
+static const char *const re = "<img\\s+((\\s*[^\"<>]+)\\s*=\\s*(\"[^\"<>]*\"\\s*))*\\s*>";
+
+int match(char * text, char * exp){
+    const char *s = text;
+    //printf("la expresion es %s\n", s);
+    regex_t regex;
+    regmatch_t pmatch[1];
+    regoff_t off, len;
+
+    if (regcomp(&regex, exp, REG_EXTENDED))
+        return -1;
+
+    printf("Coincidencias:\n");
+
+    int i;
+    for (i = 0;; i++)
+    {
+        if (regexec(&regex, s, ARRAY_SIZE(pmatch), pmatch, 0))
+            break;
+
+        off = pmatch[0].rm_so + (s - text);
+        len = pmatch[0].rm_eo - pmatch[0].rm_so;
+        printf("subcadena = \"%.*s\"\n", len, s + pmatch[0].rm_so);
+        printf("\n\n");
+
+        s += pmatch[0].rm_eo;
+    }
+    return i;
+}
+
+int matchA(char * text, char * exp){
+    
+    //abrir archivo en el cual se insertarán los links
+    const char* filename = "links.txt";
+
+    FILE* input_file = fopen(filename, "w");
+    if (!input_file)
+        return -1;
+
+    struct stat sb;
+    if (stat(filename, &sb) == -1) {
+        perror("stat");
+        return -1;
+    }
+    
+    //busco los match
+    const char *s = text;
+    //printf("la expresion es %s\n", s);
+    regex_t regex;
+    regmatch_t pmatch[1];
+    regoff_t off, len;
+
+    if (regcomp(&regex, exp, REG_EXTENDED))
+        return -1;
+
+    //printf("Coincidencias:\n");
+
+    int i;
+    for (i = 0;; i++)
+    {
+        if (regexec(&regex, s, ARRAY_SIZE(pmatch), pmatch, 0))
+            break;
+
+        off = pmatch[0].rm_so + (s - text);
+        len = pmatch[0].rm_eo - pmatch[0].rm_so;
+        fprintf(input_file, "%.*s\n", len, s + pmatch[0].rm_so);
+
+        s += pmatch[0].rm_eo;
+    }
+    fclose(input_file);
+    return i;
+}
