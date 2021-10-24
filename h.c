836a1e60b3e7b6d0befce98706384cfd6b253fd4
miguel <miguel.saavedra1601@alumnos.ubiobio.cl>
2021-10-24 19:57:22 -0300

    tiempos

---
+++ h.c
@@ -0,0 +1,19 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <inttypes.h>
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+
+void main (void)
+{
+    struct timespec begin, end;
+    clock_gettime(CLOCK_REALTIME, &begin);
+
+    int inicio = (int)(begin.tv_nsec / 1.0e6);
+
+
+    clock_gettime(CLOCK_REALTIME, &end);
+
+    int fin = (int)(begin.tv_nsec / 1.0e6);
+}
--- read.c
+++ read.c
@@ -6,12 +6,14 @@
 
 
 #include <sys/wait.h>
-
 #include <sys/types.h>
 #include <unistd.h>
 
 #include <stdint.h>
 #include <regex.h>
+
+#include <sys/time.h>
+
 
 char* leer();
 int match(char * text, char * exp );
@@ -21,49 +23,204 @@ int procesoImg(char * text);
 int procesoScript(char * text);
 int procesoA(char * text);
 
+
+
 int main(void){
 
     char * text = leer(&text);
     
-    int img = 0;
+    //contadores de etiquetas
+    int script;
+    int img;
+    int a;
 
-    //tuberia para el procesoImg
-    int fd[2];
-    pipe(fd);
+    //pipe para la cantidad de tags del procesoImg
+    int fdImg[2];
+    pipe(fdImg);
 
-    //tuberia para el procesoScript
+    //pipe para la cantidad de tags del procesoScript
+    int fdScript[2];
+    pipe(fdScript);
 
-    //tuberia para el proceso A
+    //pipe para la cantidad de tags del procesoA
+    int fdA[2];
+    pipe(fdA);
 
+    //Variable para los tiempos
+
+    //pipe para el tiempo del procesoImg
+    int fdImgT[2];
+    pipe(fdImgT);
+
+    //pipe para el tiempo del procesoScript
+    int fdScriptT[2];
+    pipe(fdScriptT);
+
+    //pipe para el tiempo del proceso A
+    int fdAT[2];
+    pipe(fdAT);
 
     int status;
 
     int f = fork();
     if(f==0){
+        //proceso hijo
 
-        close(fd[0]);
-        int n = procesoImg(text);
-        write(fd[1], &n, sizeof(int));
+        //definir variable para calcular el tiempo
+        struct timeval begin, end;
+        //calcular tiempo de inicio
+        gettimeofday(&begin, 0);
+
+        //llamar a la funcion encargada de contar la cantidad de tag IMG
+        int contImg = procesoImg(text);
+
+        for (int i = 0; i < 100000000; i++)
+                {
+                }
+
+        //calcular tiempo de fin
+        gettimeofday(&end, 0);
+
+        //calcular tiempo (está en microsegundos, se debe transformar a milisegundos)
+        printf("1..... %d  - %d \n", end.tv_usec, begin.tv_usec);
+        int seconds = (int)(end.tv_usec - begin.tv_usec)/1000;
+
+
+        //usar pipe para escribir resultados y luego pasarlo al proceso padre
+        close(fdImgT[0]);
+        write(fdImgT[1], &seconds, sizeof(int));
+        close(fdImgT[1]);
+
+        //usar pipe para escribir resultados y luego pasarlo al proceso padre
+        close(fdImg[0]);
+        write(fdImg[1], &contImg, sizeof(int));
+        close(fdImg[1]);
         
     }else{
         int s = fork();
         if(s==0){
-            int n = procesoScript(text);
-            //codigo de la etiqueta script
+
+            //definir variable para calcular el tiempo
+            struct timeval begin, end;
+            //calcular tiempo de inicio
+            gettimeofday(&begin, 0);
+
+            //llamar a la funcion encargada de contar la cantidad de tag SCRIPT
+            int contScript = procesoScript(text);
+            
+            for (int i = 0; i < 100000000; i++)
+                {
+                }
+
+            //calcular tiempo de fin
+            gettimeofday(&end, 0);
+
+            //calcular tiempo (está en microsegundos, se debe transformar a milisegundos)
+            printf("2..... %d  - %d", end.tv_usec, begin.tv_usec);
+            int seconds = (int) (end.tv_usec - begin.tv_usec)/1000;
+
+
+            //usar pipe para escribir resultados y luego pasarlo al proceso padre
+            close(fdScriptT[0]);
+            write(fdScriptT[1], &seconds, sizeof(int));
+            close(fdScriptT[1]);
+
+            //usar pipe para escribir resultados y luego pasarlo al proceso padre
+            close(fdScript[0]);
+            write(fdScript[1], &contScript, sizeof(int));
+            close(fdScript[1]);
 
         }else{
             int g = fork();
             if(g==0){
-                int n = procesoA(text);
-                //codigo de la etiqueta a
+
+                //definir variable para calcular el tiempo
+                struct timeval begin, end;
+                //calcular tiempo de inicio
+                gettimeofday(&begin, 0);
+
+                //llamar a la funcion encargada de contar la cantidad de tag A
+                int contA = procesoA(text);
+
+                for (int i = 0; i < 100000000; i++)
+                {
+                }
+                
+                //calcular tiempo de fin
+                gettimeofday(&end, 0);
+
+                
+
+                //calcular tiempo (está en microsegundos, se debe transformar a milisegundos)
+                printf("%d  - %d", end.tv_usec, begin.tv_usec);
+                int seconds = (int) (end.tv_usec - begin.tv_usec)/1000;
+
+
+                //usar pipe para escribir resultados y luego pasarlo al proceso padre
+                close(fdAT[0]);
+                write(fdAT[1], &seconds, sizeof(int));
+                close(fdAT[1]);
+
+                //usar pipe para escribir resultados y luego pasarlo al proceso padre
+                close(fdA[0]);
+                write(fdA[1], &contA, sizeof(int));
+                close(fdA[1]);
 
             }else{
+
+                //esperar a que terminen los procesos hijos
                 pid_t w = waitpid(f, &status, WUNTRACED);
+                pid_t x = waitpid(s, &status, WUNTRACED);
+                pid_t y = waitpid(g, &status, WUNTRACED);
 
-                close(fd[1]);
-                read(fd[0],&img,sizeof(int));
+                //pipe para obtener la cantidad de tags A
+                close(fdA[1]);
+                read(fdA[0],&a,sizeof(int));
+                close(fdA[0]);
+                
+                //pipe para obtener la cantidad de tags IMG
+                close(fdImg[1]);
+                read(fdImg[0],&img,sizeof(int));
+                close(fdImg[0]);
 
-                printf("las coincidencias son %d\n", img);
+                //pipe para obtener la cantidad de tags SCRIPT
+                close(fdScript[1]);
+                read(fdScript[0],&script,sizeof(int));
+                close(fdScript[0]);
+
+
+                //pipes para leer los tiempos de
+
+                //pipe para leer el tiempo de img
+                int timeImg;
+                close(fdImgT[1]);
+                read(fdImgT[0],&timeImg,sizeof(int));
+                close(fdImgT[0]);
+
+                //pipe para leer el tiempo de script
+                int timeScript;
+                close(fdScriptT[1]);
+                read(fdScriptT[0],&timeScript,sizeof(int));
+                close(fdScriptT[0]);
+
+                //pipe para leer el tiempo de a
+                int timeA;
+                close(fdAT[1]);
+                read(fdAT[0],&timeA,sizeof(int));
+                close(fdAT[0]);
+
+
+
+                //mostrar los resultados
+                printf("Estadisticas: \n");
+                printf("las coincidencias de IMG son %d\n", img);
+                printf("las coincidencias de Script son %d\n", script);
+                printf("las coincidencias de A son %d\n\n\n", a);
+
+                printf("Estadisticas del tiempo de ejecucion: \n");
+                printf("los tiempos de IMG son %d\n", timeImg);
+                printf("los tiempos de Script son %d\n", timeScript);
+                printf("los tiempos de A son %d\n", timeA);
             }
         }
 
@@ -72,30 +229,44 @@ int main(void){
 }
 
 int procesoImg(char * text){
-
+    //expresion regular para obtener tags IMG
     char * exp = "<img\\s+((\\s*[^\"<>]+)\\s*=\\s*(\"[^\"<>]*\"\\s*))*\\s*>";
 
-    int numero = matchA(text, exp);
 
-    printf("las coincidencias son %d\n", numero);
+    
+    int cantidad = match(text, exp);
+    
 
-    return numero;
+    return cantidad;
 }
 
 int procesoScript(char * text){
 
-    char * exp = "falta expresion regular";
+    //encontrar expresion regular
+    char * exp = "<img\\s+((\\s*[^\"<>]+)\\s*=\\s*(\"[^\"<>]*\"\\s*))*\\s*>";
 
-    match(text, exp);
 
+    //realizar funcionalidad del proceso
+    int cantidad = match(text, exp);
+    
+    
+    return cantidad;
 }
 
 int procesoA(char * text){
-    char * exp = "falta expresion regular";
-    
-    match(text, exp);
 
-}
+    //encontrar expresion regular
+    char * exp = "<img\\s+((\\s*[^\"<>]+)\\s*=\\s*(\"[^\"<>]*\"\\s*))*\\s*>";
+
+    
+
+    //realizar funcionalidad del proceso
+    int cantidad = match(text, exp);
+    
+    
+
+    return cantidad;
+}  
 
 char* leer() {
     
@@ -131,6 +302,7 @@ static const char *const str = "<img height=\"\" alt=\"1MDN\" srcset=\"mdn-logo-
 static const char *const re = "<img\\s+((\\s*[^\"<>]+)\\s*=\\s*(\"[^\"<>]*\"\\s*))*\\s*>";
 
 int match(char * text, char * exp){
+
     const char *s = text;
     //printf("la expresion es %s\n", s);
     regex_t regex;
@@ -140,7 +312,7 @@ int match(char * text, char * exp){
     if (regcomp(&regex, exp, REG_EXTENDED))
         return -1;
 
-    printf("Coincidencias:\n");
+    //printf("Coincidenci1as:\n");
 
     int i;
     for (i = 0;; i++)
@@ -150,20 +322,21 @@ int match(char * text, char * exp){
 
         off = pmatch[0].rm_so + (s - text);
         len = pmatch[0].rm_eo - pmatch[0].rm_so;
-        printf("subcadena = \"%.*s\"\n", len, s + pmatch[0].rm_so);
-        printf("\n\n");
+        //printf("subcadena = \"%.*s\"\n", len, s + pmatch[0].rm_so);
+        //printf("\n\n");
 
         s += pmatch[0].rm_eo;
     }
     return i;
 }
 
+//buscar comando grep 
 int matchA(char * text, char * exp){
     
     //abrir archivo en el cual se insertarán los links
     const char* filename = "links.txt";
-
     FILE* input_file = fopen(filename, "w");
+
     if (!input_file)
         return -1;
 
@@ -175,7 +348,7 @@ int matchA(char * text, char * exp){
     
     //busco los match
     const char *s = text;
-    //printf("la expresion es %s\n", s);
+
     regex_t regex;
     regmatch_t pmatch[1];
     regoff_t off, len;
@@ -183,7 +356,6 @@ int matchA(char * text, char * exp){
     if (regcomp(&regex, exp, REG_EXTENDED))
         return -1;
 
-    //printf("Coincidencias:\n");
 
     int i;
     for (i = 0;; i++)
@@ -193,6 +365,9 @@ int matchA(char * text, char * exp){
 
         off = pmatch[0].rm_so + (s - text);
         len = pmatch[0].rm_eo - pmatch[0].rm_so;
+
+        //encontrar subcadena para obtener los links
+        
         fprintf(input_file, "%.*s\n", len, s + pmatch[0].rm_so);
 
         s += pmatch[0].rm_eo;
